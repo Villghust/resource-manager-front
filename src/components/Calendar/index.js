17dbb5409fd1ddba884b90efdaf2656fd665fc0e
Otavio Bonder <otaviobonder@gmail.com>
2020-06-03 16:04:44 -0300

    added default functions to scheduler component

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,53 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { EditingState } from '@devexpress/dx-react-scheduler';
+import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
     Appointments,
     AppointmentForm,
+    DayView,
+    MonthView,
     Scheduler,
+    Toolbar,
     WeekView,
+    ViewSwitcher,
+    DateNavigator,
 } from '@devexpress/dx-react-scheduler-material-ui';
+import { Paper } from '@material-ui/core';
+import moment from 'moment';
 
 const dummyData = [
     {
-        _id: '5eba1f7633b9a19c110276cc',
-        name: 'Otavio',
-        apartment: '1309',
-        startDate: '2020-05-11T18:25:43.511Z',
-        endDate: '2020-05-12T18:25:43.511Z',
-        __v: 0,
-    },
-    {
-        _id: '5eba1f7733b9a19c110276cd',
-        name: 'Otavio',
-        apartment: '1309',
-        startDate: '2020-05-11T18:25:43.511Z',
-        endDate: '2020-05-12T18:25:43.511Z',
-        __v: 0,
-    },
-    {
         _id: '5eba1f7833b9a19c110276ce',
-        name: 'Otavio',
-        apartment: '1309',
-        startDate: '2020-05-11T18:25:43.511Z',
-        endDate: '2020-05-12T18:25:43.511Z',
-        __v: 0,
+        title: 'Appointment',
+        startDate: '2020-06-03T20:25:43.511Z',
+        endDate: '2020-06-03T21:25:43.511Z',
     },
     {
         _id: '5eba1f7833b9a19c110276cf',
-        name: 'Otavio',
-        apartment: '1309',
-        startDate: '2020-05-11T18:25:43.511Z',
-        endDate: '2020-05-12T18:25:43.511Z',
-        __v: 0,
+        title: 'Appointment',
+        startDate: '2020-06-03T18:25:43.511Z',
+        endDate: '2020-06-03T19:25:43.511Z',
     },
 ];
 
 export default function Schedule() {
+    // set the view state of the scheduler component
+    const [viewState, setViewState] = useState('week');
+
+    // manage the date to show on the scheduler
+    const [date, setDate] = useState(moment()._d);
+
+    // change the view state
+    const handleViewChange = (newView) => {
+        setViewState(newView);
+    };
+
+    // change the date to display on the scheduler
+    const handleDateChange = (newDate) => {
+        setDate(newDate);
+    };
+
     return (
-        <Scheduler data={dummyData}>
-            <EditingState />
-            <WeekView />
-        </Scheduler>
+        <Paper>
+            <Scheduler data={dummyData}>
+                <ViewState
+                    currentViewName={viewState}
+                    onCurrentViewNameChange={handleViewChange}
+                    currentDate={date}
+                    onCurrentDateChange={handleDateChange}
+                />
+                <DayView name="day" />
+                <WeekView name="week" />
+                <MonthView name="month" />
+                <Toolbar />
+                <DateNavigator />
+                <ViewSwitcher />
+                <Appointments />
+            </Scheduler>
+        </Paper>
     );
 }
